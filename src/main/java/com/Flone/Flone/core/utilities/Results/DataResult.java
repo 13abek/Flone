af15e36527d48d21ef1ffe56e6de40862c08db10
unknown <13abek>
2021-06-18 06:05:54 +0400

    3Refactoring

--- src/main/java/com/Flone/Flone/core/utilities/Results/DataResult.java
+++ src/main/java/com/Flone/Flone/core/utilities/Results/DataResult.java
@@ -7,11 +7,13 @@ public class DataResult<T> extends Result{
         super(success,message);
         this.data=data;
     }
+
     public DataResult(T data,boolean success){
         super(success);
         this.data=data;
     }
-    public T getData(T data){
-      return   this.data=data;
+    public T getData(){
+      return this.data;
     }
+
 }
