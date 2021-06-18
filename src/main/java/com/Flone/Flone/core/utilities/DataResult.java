d17647d53fd0fc632ecaf90f0074bd386a96cf7b
unknown <13abek>
2021-06-18 04:41:04 +0400

    DataREsponseFeature

---
+++ src/main/java/com/Flone/Flone/core/utilities/DataResult.java
@@ -0,0 +1,17 @@
+package com.Flone.Flone.core.utilities;
+
+public class DataResult<T> extends Result{
+    private T data;
+
+    public DataResult(T data,boolean success,String message){
+        super(success,message);
+        this.data=data;
+    }
+    public DataResult(T data,boolean success){
+        super(success);
+        this.data=data;
+    }
+    public T getData(T data){
+      return   this.data=data;
+    }
+}
