2e616ee77fb3357e2c2e3675366c9eaab4e7f8ea
unknown <13abek>
2021-06-25 14:54:01 +0400

    FeedbackDaoCreate

---
+++ src/main/java/com/Flone/Flone/dataAccess/abstracts/FeedbackDao.java
@@ -0,0 +1,8 @@
+package com.Flone.Flone.dataAccess.abstracts;
+
+import com.Flone.Flone.entities.concretes.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackDao extends JpaRepository<Feedback,Integer> {
+
+}
