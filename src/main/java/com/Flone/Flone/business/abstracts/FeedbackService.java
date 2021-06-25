62c5d04cdbebaf350202e3eb8e81889e9008fcfc
unknown <13abek>
2021-06-25 15:04:19 +0400

    datatypeChanges

--- src/main/java/com/Flone/Flone/business/abstracts/FeedbackService.java
+++ src/main/java/com/Flone/Flone/business/abstracts/FeedbackService.java
@@ -1,4 +1,17 @@
 package com.Flone.Flone.business.abstracts;
 
+import com.Flone.Flone.core.utilities.Results.DataResult;
+import com.Flone.Flone.core.utilities.Results.Result;
+import com.Flone.Flone.entities.concretes.Feedback;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
 public interface FeedbackService {
+    DataResult<List<Feedback>> getAll();
+    DataResult<Feedback> findById(int id);
+    Result add(Feedback feedback);
+    Result delete(Feedback feedback);
+    Result update(Feedback feedback);
+
 }
---
+++ src/main/java/com/Flone/Flone/business/concretes/FeedbackManager.java
@@ -0,0 +1,47 @@
+package com.Flone.Flone.business.concretes;
+
+import com.Flone.Flone.business.abstracts.FeedbackService;
+import com.Flone.Flone.core.utilities.Results.DataResult;
+import com.Flone.Flone.core.utilities.Results.Result;
+import com.Flone.Flone.core.utilities.Results.SuccessDataResult;
+import com.Flone.Flone.dataAccess.abstracts.FeedbackDao;
+import com.Flone.Flone.entities.concretes.Feedback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FeedbackManager implements FeedbackService {
+    private FeedbackDao feedbackDao;
+    @Autowired
+    public FeedbackManager(FeedbackDao feedbackDao){
+        this.feedbackDao=feedbackDao;
+    }
+
+
+    @Override
+    public DataResult<List<Feedback>> getAll() {
+        return new SuccessDataResult<>(this.feedbackDao.findAll(),"Feedback lists!");
+    }
+
+    @Override
+    public DataResult<Feedback> findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Result add(Feedback feedback) {
+        return null;
+    }
+
+    @Override
+    public Result delete(Feedback feedback) {
+        return null;
+    }
+
+    @Override
+    public Result update(Feedback feedback) {
+        return null;
+    }
+}
